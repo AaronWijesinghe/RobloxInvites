77943bda7995bf781c0da63837a6fe2d41ce9023
Aaron Wijesinghe <aaronw1975@icloud.com>
2026-06-18 12:49:33 -0400

    feat: add weekly stat leaderboard generation

--- summarize_stats.py
+++ summarize_stats.py
@@ -69,10 +69,82 @@ def generate_stats(playtimes, game_playtimes):
         print(leaderboard(f"[#{i}] {game}: {playtime / 3600:.2f}h", i))
     input("\nPress ENTER to return to the main menu. ")
 
+def save_data(total, playtimes, game_playtimes):
+    playtimes_diff = {}
+    game_playtimes_diff = {}
+    total_diff = 0
+    if len(data["weeks"]) > 0:
+        total_diff = data["weeks"][len(data["weeks"]) - 1]["total"] - data["weeks"][len(data["weeks"]) - 2]["total"]
+        playtimes_new = data["weeks"][len(data["weeks"]) - 1]["playtimes"]
+        playtimes_old = data["weeks"][len(data["weeks"]) - 2]["playtimes"]
+        game_playtimes_new = data["weeks"][len(data["weeks"]) - 1]["game_playtimes"]
+        game_playtimes_old = data["weeks"][len(data["weeks"]) - 2]["game_playtimes"]
+        for name in playtimes_new.keys():
+            if name in playtimes_old:
+                playtimes_diff[name] = playtimes_new[name] - playtimes_old[name]
+            else:
+                playtimes_diff[name] = playtimes_new[name]
+        for name in game_playtimes_new.keys():
+            if name in game_playtimes_old:
+                game_playtimes_diff[name] = game_playtimes_new[name] - game_playtimes_old[name]
+            else:
+                game_playtimes_diff[name] = game_playtimes_new[name]
+    else:
+        playtimes_diff = playtimes
+        game_playtimes_diff = game_playtimes
+        total_diff = total
+
+    data["weeks"] += [{
+        "total": total,
+        "playtimes": playtimes,
+        "game_playtimes": game_playtimes,
+        "total_diff": total_diff,
+        "playtimes_diff": playtimes_diff,
+        "game_playtimes_diff": game_playtimes_diff
+    }]
+    open("./server/stat_summarizer.json", "w").write(json.dumps(data, indent=2))
+
+def generate_weekly_stats():
+    total = data["weeks"][len(data["weeks"]) - 1]["total_diff"]
+    playtimes = data["weeks"][len(data["weeks"]) - 1]["playtimes_diff"]
+    game_playtimes = data["weeks"][len(data["weeks"]) - 1]["game_playtimes_diff"]
+
+    clear()
+    print(f"{gold}{bold}[Weekly Server Leaderboard]{end}")
+    print(f"{bold}{underline}Total Server Playtime This Week:{end} {total / 3600:.2f}h\n")
+
+    print(f"{bold}Weekly Playtime for Top 20 Users:{end}")
+    playtimes = sorted(playtimes.items(), key=lambda item: item[1], reverse=True)[:20]
+    for i, (user, playtime) in enumerate(playtimes, start=1):
+        print(leaderboard(f"[#{i}] {user_dict[int(user)]["username"]} ({playtime / 3600:.2f}h)", i))
+
+    print(f"\n{bold}Weekly Playtime for Top 20 Games:{end}")
+    game_playtimes = sorted(game_playtimes.items(), key=lambda item: item[1], reverse=True)[:20]
+    for i, (game, playtime) in enumerate(game_playtimes, start=1):
+        print(leaderboard(f"[#{i}] {game}: {playtime / 3600:.2f}h", i))
+    input("\nPress ENTER to return to the main menu. ")
+
+if os.path.exists("./server/stat_summarizer.json"):
+    data = json.loads(open("./server/stat_summarizer.json", "r").read())
+else:
+    data = {
+        "version": 1,
+        "weeks": []
+    }
+    open("./server/stat_summarizer.json", "w").write(json.dumps(data, indent=2))
+
 while True:
     clear()
-    print(f"{gold}{bold}[Playtime Stat Generator v3.0.0]{end}")
-    command = input("Enter a command ('generate'): ").lower().strip()
+    print(f"{gold}{bold}[Playtime Stat Generator v4.0.0]{end}")
+    print("Supports Roblox Invites v5.0.0 - v5.0.1")
+    command = input("Enter a command ('/lb'): ").lower().strip()
 
-    if command == "generate":
-        generate_stats(playtimes, game_playtimes)
+    args = command.split(" ")[1:]
+    if command.startswith("/lb"):
+        if len(args) == 0:
+            generate_stats(playtimes, game_playtimes)
+            continue
+        if args[0] == "weekly":
+            generate_weekly_stats()
+    elif command == "/save":
+        save_data(total, playtimes, game_playtimes)
