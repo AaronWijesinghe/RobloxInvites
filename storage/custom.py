18ca1f218ba21977a400ee3a31dcd6da17e5f212
Aaron Wijesinghe <aaronw1975@icloud.com>
2026-07-09 08:36:49 -0400

    fix: fix custom titles from not applying immediately after they are added

--- storage/custom.py
+++ storage/custom.py
@@ -13,21 +13,19 @@ class CGTManager:
             await self.api.cache_id(place_id)
         universe_id = self.api.cache["indexes"][place_id]
 
-        ct = load_data("custom_titles.json", {"version": 1, "titles": {}})
-        ct["titles"][str(universe_id)] = {
+        self.custom_titles["titles"][str(universe_id)] = {
             "title": title,
             "color": hex_color,
             "game": await self.api.get_game_name(place_id),
             "place_id": place_id,
         }
-        save_data(ct, "custom_titles.json")
+        save_data(self.custom_titles, "custom_titles.json")
 
     async def remove_custom_title(self, place_id):
         if place_id not in self.api.cache["indexes"]:
             await self.api.cache_id(place_id)
         universe_id = self.api.cache["indexes"][place_id]
         
-        ct = load_data("custom_titles.json", {"version": 1, "titles": {}})
-        if str(universe_id) in ct["titles"]:
-            del ct["titles"][str(universe_id)]
-        save_data(ct, "custom_titles.json")
+        if str(universe_id) in self.custom_titles["titles"]:
+            del self.custom_titles["titles"][str(universe_id)]
+        save_data(self.custom_titles, "custom_titles.json")
