865191acf85b27337793aa10d726456659775879
Aaron Wijesinghe <aaronw1975@icloud.com>
2026-07-21 12:35:55 -0400

    feat: add a link to edit your description

--- database/users.py
+++ database/users.py
@@ -111,7 +111,7 @@ class UserManager:
             if not user_exists_in_ri:
                 user_data = await self.api.get_misc(f"https://users.roblox.com/v1/users/{user_id}")
                 if user_data["description"].lower().strip() != "i confirm that i am joining the invites program.":
-                    return f"You must verify that the following account (@{username}) is yours.\nPlease set `I confirm that I am joining the Invites program.` as your Roblox account description and retry again."
+                    return f"You must verify that the following account (@{username}) is yours.\nPlease set `I confirm that I am joining the Invites program.` as your Roblox account description and retry again.\nYou can edit your description [here](<https://www.roblox.com/users/profile/edit>)."
             elif user_exists_in_guild:
                 return f"This user already exists in this server."
             else:
