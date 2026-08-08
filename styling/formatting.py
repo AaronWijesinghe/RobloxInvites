4e9d90ac3a251f560a78fc08896dae1cfb4f5832
Aaron Wijesinghe <aaronw1975@icloud.com>
2026-08-07 21:25:20 -0400

    chore: remove unused formatting code

--- styling/formatting.py
+++ styling/formatting.py
@@ -7,16 +7,3 @@ def game_ends_in_punctuation(game_name):
             return False
     except:
         return False
-
-def get_number(string):
-    new_string = ""
-    scanning_place_id = False
-    for char in string:
-        if char.isdigit() and not scanning_place_id:
-            scanning_place_id = True
-            new_string += char
-        elif char.isdigit() and scanning_place_id:
-            new_string += char
-        elif not char.isdigit() and scanning_place_id:
-            break
-    return int(new_string)
