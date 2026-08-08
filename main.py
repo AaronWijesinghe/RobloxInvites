d3c1f1314df2cc59cb2fb61ae8016b5c85a25e00
Aaron Wijesinghe <aaronw1975@icloud.com>
2026-08-08 12:29:49 -0400

    chore: update version to 2.4.1

--- main.py
+++ main.py
@@ -10,32 +10,12 @@ headers = {
     "Cookie": f".ROBLOSECURITY={os.environ["cookie"]}"
 }
 
-version = "2.4.0"
+version = "2.4.1"
 patch_notes = """
 Updated from __v{0}__ to __v{1}__
 
 **Patch Notes:**
-- Added admin commands
-    - These commands can only be run by the bot owner.
-   - Run `/admin backup` to create a server backup.
-   - Run `/admin remove` to remove any user from Roblox Invites.
-   - Run `/admin announce` to send an announcement to all announcement channels.
-- Added a new invite link service at https://ropresencetools.github.io/
-    - This should be much more reliable than https://rblxevents.co, which went down recently
-- Added the ability to freeze your account
-    - This stops sending invite messages to all servers you are in and stops accumulating playtime.
-   - Run `/user freeze` to enable this privacy control.
-   - Run `/user unfreeze` to disable this privacy control.
-- Added the ability to hide invite messages in a specific server
-    - This stops sending invite messages to a specific server, but still lets you accumulate playtime.
-   - Run `/server pause_invites` to enable this privacy control.
-   - Run `/server resume_invites` to disable this privacy control.
-- Added update announcements like this one
-- Fixed an issue where usercards would show the incorrect position for the Since Last Snapshot leaderboard
-- Fixed an issue where leaving a server wouldn't properly unlink that user from that server
-- Fixed the text in the Remove Blacklist success embed
-- Optimized searching within commands
-- Roblox Invites is now a part of RoPresenceTools (GitHub)
+- Automatically create the backups folder so the bot doesn't crash when trying to make backups
 """
 
 if not os.path.exists("./database/backups/"):
